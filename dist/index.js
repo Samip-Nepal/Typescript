07c3bd970a9004c9be6805e256924ef9869ebc56
Samip Nepal <samipnepal5@gmail.com>
2024-09-15 16:07:16 +1000

    Array in ts

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,6 @@ let ishero = true;
 function hi_there(document) {
     console.log(document);
 }
+let number = [1, 2, 3, 4];
+number.forEach((num) => (console.log(num)));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ let age=25;
 let first_name="samip";
 let ishero= true;
 
-function hi_there(document) {
+function hi_there(document:any) {
     console.log(document);//document is not define variable can do any 
-    
 }
+
+//let number:number[]=[1,2,3,"2"];//string cannot be place
+let number=[1,2,3,4];
+number.forEach((num)=>(console.log(num)));
